b2766d79451152a3da7ba7d2257a95b2ddab8ae3
jngaravitoc <jngaravitoc@email.arizona.edu>
2025-12-22 11:17:17 -0500

    removing kinematic functionality

--- nba/__init__.py
+++ nba/__init__.py
@@ -2,7 +2,7 @@ from .decorators import requires_library
 from . import ios
 from . import com
 from . import structure
-from . import kinematics
+#from . import kinematics
 #from . import orbits
 #from . import visuals
 #from . import cosmology
